bc6eae78d15bda9066469ef06bdd745db6af70cd
jhinchley <dev.jjh@planet-hinchley.com>
2017-06-14 21:14:24 -0500

    search bar implemented and included in app

---
+++ src/containers/search_bar.js
@@ -0,0 +1,41 @@
+/**
+ * Created by jhinchley on 6/14/17.
+ */
+
+import React,{Component} from 'react';
+export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            term:''
+        }
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    render(){
+        return(
+            <form
+                className="input-group"
+                onSubmit={this.onFormSubmit}
+            >
+                <input
+                    placeholder="Get a 5-day forecast in your favorite city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+}
