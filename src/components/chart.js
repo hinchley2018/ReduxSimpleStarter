8337b1804300bf5f873a70a7b9f9d147b371a216
jhinchley <dev.jjh@planet-hinchley.com>
2017-06-17 09:52:48 -0500

    Averages and unit conversions

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-
-import {Sparklines,SparklinesLine} from 'react-sparklines';
+import _ from 'lodash';
+import {Sparklines,SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
 
 export default (props) => {
+
+    function average(data) {
+        return _.round(_.sum(data)/data.length);
+    }
 
     return (
         <div>
             <Sparklines height={120} width={120} data={props.data} >
                 <SparklinesLine color={props.color}/>
+                <SparklinesReferenceLine type="avg"/>
             </Sparklines>
+            <div>Avg: {average(props.data)} ({props.units})</div>
         </div>
     );
 
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -29,20 +29,21 @@ class WeatherList extends Component {
 
     renderWeather(cityData) {
         const name = cityData.city.name;
-        const temps = cityData.list.map(weather => weather.main.temp);
+        //convert temp to celisus
+        const temps = _.map(cityData.list.map(weather => weather.main.temp),(temp) => temp-273);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
         return(
             <tr key={name}>
                 <td>{name}</td>
                 <td>
-                    <Chart data={temps} color="red"/>
+                    <Chart data={temps} color="red" units="C"/>
                 </td>
                 <td>
-                    <Chart data={pressures} color="green"/>
+                    <Chart data={pressures} color="green" units="hPa"/>
                 </td>
                 <td>
-                    <Chart data={humidities} color="blue"/>
+                    <Chart data={humidities} color="blue" units="%"/>
                 </td>
             </tr>
         )
